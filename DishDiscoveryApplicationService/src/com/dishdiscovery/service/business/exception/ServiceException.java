50dd299191c895ff9bea77fb1c49f0427a028b46
dksingh04 <deepaksingh04@gmail.com>
2015-01-21 09:52:31 +0530

    added serial version id

--- DishDiscoveryApplicationService/src/com/dishdiscovery/service/business/exception/ServiceException.java
+++ DishDiscoveryApplicationService/src/com/dishdiscovery/service/business/exception/ServiceException.java
@@ -2,4 +2,9 @@ package com.dishdiscovery.service.business.exception;
 
 public class ServiceException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
